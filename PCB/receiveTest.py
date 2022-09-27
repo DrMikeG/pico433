cbbf3c2643ba50dedc00898476340a21ef571328
Mike Gibbens <mike.gibbens@siemens.com>
2022-09-27 17:38:08 +0100

    Updated receive test.py with test mode

--- PCB/receiveTest.py
+++ PCB/receiveTest.py
@@ -10,8 +10,9 @@ onBoardLed.value(0)
 blueLed= Pin(18, Pin.OUT)
 blueLed.value(0)
 
-touchSensorPinActiveHigh= Pin(19,Pin.IN) # directly connected to touch pad
+touchSensorPinActiveHigh= Pin(19,Pin.IN, Pin.PULL_DOWN) # directly connected to touch pad
 
+# Use this to print the contents of remotes
 #recv = RX(pin())
 #recv.load('remotes')
 #print(recv.keys())
@@ -19,29 +20,33 @@ touchSensorPinActiveHigh= Pin(19,Pin.IN) # directly connected to touch pad
 #print(recv.show('2off'))
 
 transmit = TX(Pin(16, Pin.OUT), 'remotes', reps=10)
-sleep(1)
+sleep(2)
 
-lightOn = True
+testMode = True
+
+switchOnCmd = 'L1On'
+switchOffCmd = 'L1Off'
+
+if testMode:
+    switchOnCmd = '2on'
+    switchOffCmd = '2off'
+
+lightOn = False
 
 while True:
     
     if touchSensorPinActiveHigh.value():
-        if lightOn:
-            print("Transmit light on")
-            transmit('L1On')  # Immediate return
-            lightOn = False
-            blueLed.value(1)
-            sleep(1)
-        else:
-            print("Transmit light off")
-            transmit('L2Off')  # Immediate return
-            lightOn = True
-            blueLed.value(0)
-            sleep(1)
-    
-    
-
-
-
-
-
+        sleep(0.1) # debounce
+        if touchSensorPinActiveHigh.value():
+            if lightOn:
+                print("Transmit light off")                
+                transmit(switchOffCmd)  # Immediate return
+                lightOn = False
+                blueLed.value(0)
+                sleep(1) # prevent chatter
+            else:
+                print("Transmit light on")
+                transmit(switchOnCmd)                
+                lightOn = True
+                blueLed.value(1)
+                sleep(1)
