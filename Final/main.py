3e2b3ca8a66990336712b30b4013bd246e518bcc
Mike Gibbens <mike.gibbens@siemens.com>
2022-11-05 14:48:01 +0000

    final mail.py is running out of memory after opening 3rd connection

--- Final/main.py
+++ Final/main.py
@@ -46,157 +46,183 @@ pw = secrets['pw']
 transmit = TX(machine.Pin(16, machine.Pin.OUT), 'remotes', reps=10)
 time.sleep(2)
 
+response = get_html('index.html')
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+# Request fixed IP address of 192.168.0.97
+wlan.ifconfig(('192.168.0.97', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
+
 while True:
 
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    # Request fixed IP address of 192.168.0.97
-    wlan.ifconfig(('192.168.0.97', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
-    wlan.connect(ssid, pw)
+    try :
+        wlan_status = wlan.status()
+        if wlan_status != 3:
 
-    # Wait for connection with 10 second timeout
-    timeout = 10
-    while timeout > 0:
-        if wlan.status() < 0 or wlan.status() >= 3:
-            break
-        timeout -= 1
-        print('Waiting for connection...')
-        time.sleep(1)
+            wlan.connect(ssid, pw)
 
-    wlan_status = wlan.status()
-    blink_onboard_led(wlan_status)
+            # Wait for connection with 10 second timeout
+            timeout = 10
+            while timeout > 0:
+                if wlan.status() < 0 or wlan.status() >= 3:
+                    break
+                timeout -= 1
+                print('Waiting for connection...')
+                time.sleep(1)
 
-    if wlan_status != 3:
-        #raise RuntimeError('Wi-Fi connection failed')
-        print("Wi-Fi connection failed")
-        time.sleep(180)
-        break
-    else:
-        print('Connected')
-        status = wlan.ifconfig()
-        print('ip = ' + status[0])
+            wlan_status = wlan.status()
+            blink_onboard_led(wlan_status)
 
-    # HTTP server with socket
-    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-
-    # Create a "server socket":
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(addr)
-    server_socket.listen(1)
-    # The argument to listen tells the socket library that we want it to queue up 1 connect requests (the normal max) before refusing outside connections. 
-    print("Listening on {} ".format(addr))
-
-    led = machine.Pin('LED', machine.Pin.OUT)
-
-    testMode = True
-
-    switchOnCmd = 'L1On'
-    switchOffCmd = 'L1Off'
-
-    if testMode:
-        switchOnCmd = '2on'
-        switchOffCmd = '2off'
-
-    lightOn = False
-
-    inputs = [server_socket]
-    outputs = [server_socket]
-    while inputs:
-        #print('Non Blocking - waiting...')
-        #print('.', end='')
-
-        #print("Inputs list contains {} sockets".format(len(inputs)))
-        readable,writable,exceptional = select.select(inputs,outputs,inputs,0.5)   
-        #print("Select[readable,writable,exceptional] = {},{},{}".format(len(readable),len(writable),len(exceptional)))
-
-        for s in writable:
-            print("Client socket is ready for a response")
-            # Send the stock response
-            print('Sending index.html')
-            response = get_html('index.html')
-            s.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-            s.sendall(response)
-            # Close the connection
-            s.close()
-            print("Closing socket")
-            #print('Sent {} of {} bytes'.format(bytesSent,len(response)))
-            # Do not select from this socket
-            if s in inputs:
-                inputs.remove(s)
-            if s in outputs:
-                outputs.remove(s)
-            break
-
-        for s in readable:
-            # Is there data on the server socket?
-            # If so, we can use accept to get client_socket
-            if s is server_socket:
-                print("Server socket is readable")
-                client_socket, address = server_socket.accept()
-                print("Connection from {}".format(address))
-                print("Adding to input list to read from")
-                # check if there is data on the client socket?
-                inputs.append(client_socket)            
+            if wlan_status != 3:
+                #raise RuntimeError('Wi-Fi connection failed')
+                print("Wi-Fi connection failed")
+                time.sleep(30)
+                continue
             else:
-                print("Client socket is readable")
-                # We have a client socket
-                print('Reading client socket')
-                data = s.recv(1024)
-                print('Read {} bytes'.format(len(data)))
-                # It is your responsibility to call recv again until your message has been completely dealt with.
-                # A protocol like HTTP uses a socket for only one transfer. The client sends a request, then reads a reply.
-                # That’s it. The socket is discarded. This means that a client can detect the end of the reply by receiving 0 bytes.            
-                if data:
-                    print("Client socket had data / more data")
-                    r = str(data[:50])
-                    print(r)
-                    led_on = r.find('?led=on')
-                    led_off = r.find('?led=off')
-                    print('led_on = ', led_on)
-                    print('led_off = ', led_off)
-                    if led_on > -1:
-                        print("Transmit light on")
-                        transmit(switchOnCmd)                
-                        lightOn = True
-                        blueLed.value(1)
+                print('Connected')
+                status = wlan.ifconfig()
+                print('ip = ' + status[0])
+        else:
+            print("Wifi already connected, reopening server socket")
+
+        # HTTP server with socket
+        addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+        # Create a "server socket":
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind(addr)
+        server_socket.listen(5)
+        # The argument to listen tells the socket library that we want it to queue up 1 connect requests (the normal max) before refusing outside connections. 
+        print("Listening on {} ".format(addr))
+
+        led = machine.Pin('LED', machine.Pin.OUT)
+
+        testMode = True
+
+        switchOnCmd = 'L1On'
+        switchOffCmd = 'L1Off'
+
+        if testMode:
+            switchOnCmd = '2on'
+            switchOffCmd = '2off'
+
+        lightOn = False
+
+        inputs = [server_socket]
+        outputs = [server_socket]
+        while inputs:
+            #print('Non Blocking - waiting...')
+            #print('.', end='')
+
+            #print("Inputs list contains {} sockets".format(len(inputs)))
+            readable,writable,exceptional = select.select(inputs,outputs,inputs,0.5)   
+            #print("Select[readable,writable,exceptional] = {},{},{}".format(len(readable),len(writable),len(exceptional)))
+
+            for s in writable:
+                print("Client socket is ready for a response")
+                try :
+                    # Send the stock response
+                    print('Sending index.html')                
+                    s.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+                    print('Header sent')
+                    s.sendall(response)
+                    print('Response sent')
+                    # Close the connection
+                    s.close()
+                    print("Closing socket")
+                    #print('Sent {} of {} bytes'.format(bytesSent,len(response)))
+                    # Do not select from this socket
+                except Exception as error:
+                    print(error)
+                finally:
+                    if s in inputs:
+                        inputs.remove(s)
+                    if s in outputs:
+                        outputs.remove(s)
+                    continue
+
+            for s in readable:
+                # Is there data on the server socket?
+                # If so, we can use accept to get client_socket
+                if s is server_socket:
+                    print("Server socket is readable")
+                    client_socket, address = server_socket.accept()
+                    print("Connection from {}".format(address))
+                    print("Adding to input list to read from")
+                    # check if there is data on the client socket?
+                    inputs.append(client_socket)            
+                else:
+                    print("Client socket is readable")
+                    # We have a client socket
+                    print('Reading client socket')
+                    data = s.recv(1024)
+                    print('Read {} bytes'.format(len(data)))
+                    # It is your responsibility to call recv again until your message has been completely dealt with.
+                    # A protocol like HTTP uses a socket for only one transfer. The client sends a request, then reads a reply.
+                    # That’s it. The socket is discarded. This means that a client can detect the end of the reply by receiving 0 bytes.            
+                    if data:
+                        print("Client socket had data / more data")
+                        r = str(data[:50])
+                        print(r)
+                        led_on = r.find('?led=on')
+                        led_off = r.find('?led=off')
+                        led_toggle = r.find('?led=toggle')
+                        print('led_on = ', led_on)
+                        print('led_off = ', led_off)
+                        print('led_toggle = ', led_toggle)
                         
-                    if led_off > -1:
+                        if led_toggle > -1:
+                            if lightOn:
+                                led_off = 10
+                            else:
+                                led_on = 10
+                        
+                        if led_on > -1:
+                            print("Transmit light on")
+                            transmit(switchOnCmd)                
+                            lightOn = True
+                            blueLed.value(1)
+                            
+                        if led_off > -1:
+                            print("Transmit light off")                
+                            transmit(switchOffCmd)  # Immediate return
+                            lightOn = False
+                            blueLed.value(0)
+                    else:
+                        print("Couldn't read from client socket")
+                        # Client socket now becomes an output
+                        if s in inputs:
+                            inputs.remove(s)        
+                    
+                    # Put the socket in the writables list, even if it's still get data to read?
+                    if not s in outputs:
+                        outputs.append(s)
+
+            for s in exceptional:
+                print('Non Blocking - error in {}'.format(s))
+                if s in inputs:
+                    inputs.remove(s)
+                if s in outputs:
+                    outputs.remove(s)
+                break
+            
+            print('.', end='')
+            if touchSensorPinActiveHigh.value():
+                time.sleep(0.1) # debounce
+                if touchSensorPinActiveHigh.value():
+                    if lightOn:
                         print("Transmit light off")                
                         transmit(switchOffCmd)  # Immediate return
                         lightOn = False
                         blueLed.value(0)
-                else:
-                    print("Couldn't read from client socket")
-                    # Client socket now becomes an output
-                    if s in inputs:
-                        inputs.remove(s)        
-                
-                # Put the socket in the writables list, even if it's still get data to read?
-                if not s in outputs:
-                    outputs.append(s)
-
-        for s in exceptional:
-            print('Non Blocking - error in {}'.format(s))
-            if s in inputs:
-                inputs.remove(s)
-            if s in outputs:
-                outputs.remove(s)
-            break
-        
-        print('.', end='')
-        if touchSensorPinActiveHigh.value():
-            time.sleep(0.1) # debounce
-            if touchSensorPinActiveHigh.value():
-                if lightOn:
-                    print("Transmit light off")                
-                    transmit(switchOffCmd)  # Immediate return
-                    lightOn = False
-                    blueLed.value(0)
-                    time.sleep(1) # prevent chatter
-                else:
-                    print("Transmit light on")
-                    transmit(switchOnCmd)                
-                    lightOn = True
-                    blueLed.value(1)
-                    time.sleep(1)
+                        time.sleep(1) # prevent chatter
+                    else:
+                        print("Transmit light on")
+                        transmit(switchOnCmd)                
+                        lightOn = True
+                        blueLed.value(1)
+                        time.sleep(1)
+    
+    except Exception as error:
+        print(error)
--- README.md
+++ README.md
@@ -5,3 +5,13 @@ project to replace esp32 433mhz controller with pi pico
 
 Micropython needs script called main.py to auto-run.
 
+
+
+
+https://forums.raspberrypi.com/viewtopic.php?t=340099
+
+Your method of clearing PIO state has one major drawback in that you're not clearing PIO memory, so inevitably you'll run into ENOMEM error because MicroPython will add instructions to memory instead of overwriting them. You also need to run:
+
+PIO(x).remove_program()
+
+I've tested it a bit and you're absolutely right. It seems that on Pico non-W (haven't tested on W) pressing play or stop (or equivalent keyboard shortcuts) in Thonny clears the PIO memory but keeps the state machines running and pins configured to PIO function, so loading the same program in the same instruction space will make all previously running SMs run again as if nothing happened. I think it's a major oversight and a quick glance at issues on MicroPython's github doesn't show anything similar, so maybe it's worth filing an issue there.
